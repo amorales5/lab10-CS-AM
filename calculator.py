199eabd14edbe455e0ee14d945ea0c91954e7eaa
amorales5 <alexsis.morales@gmail.com>
2025-07-31 10:15:50 -1000

    Partner 2 tasks

--- calculator.py
+++ calculator.py
@@ -4,8 +4,25 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    try:
+        return b / a  #raise ZeroDivisionError if a == 0
+    except ZeroDivisionError:
+        print("Division by zero")
+def log(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        print("ValueError")
+def exp(a, b):
+    return a ** b
 
 
